c0297c6641f62e90ba40b36bec9bd08addc26057
dineshkumar0001 <dinesh.kumar@quiksync.com>
2023-08-23 22:45:37 +0530

    remove widget

--- src/app/components/add-page/add-page.component.ts
+++ src/app/components/add-page/add-page.component.ts
@@ -6,12 +6,19 @@ import 'gridstack'; // Import Gridstack JavaScript
 import { Router } from '@angular/router';
 import { Widget } from '../../models/widget.model';
 import { WidgetService } from '../../services/widget.service';
-import { GridStack, GridStackOptions } from "gridstack";
+import { GridStack, GridStackOptions, GridItemHTMLElement } from "gridstack";
 
 import { PagesService } from '../../services/pages.service';
 import { Location } from '@angular/common';
-import { GridStackWidget, GridStackNode } from "gridstack/dist/types";
+import { GridStackWidget, GridStackNode, GridStackElement } from "gridstack/dist/types";
 import { DDElement } from "gridstack/dist/dd-element";
+
+
+
+// NOTE: local testing of file
+// import { GridstackComponent, NgGridStackOptions, NgGridStackWidget, elementCB, gsCreateNgComponents, nodesCB } from './gridstack.component';
+import { GridstackComponent, NgGridStackOptions, NgGridStackWidget, elementCB, gsCreateNgComponents, nodesCB } from 'gridstack/dist/angular';
+
 
 // Define the GridMode type
 type GridMode = "edit" | "view";
@@ -23,6 +30,11 @@ type GridMode = "edit" | "view";
   styleUrls: ['./add-page.component.css']
 })
 export class AddPageComponent implements OnInit, AfterViewInit {
+
+
+  @ViewChild(GridstackComponent) gridComp?: GridstackComponent;
+  @ViewChild('origTextArea', { static: true }) origTextEl?: ElementRef<HTMLTextAreaElement>;
+  @ViewChild('textArea', { static: true }) textEl?: ElementRef<HTMLTextAreaElement>;
 
   // Initialize variables
   widget: Widget[] = [];
@@ -51,11 +63,7 @@ export class AddPageComponent implements OnInit, AfterViewInit {
     draggable: {
       
       handle: '.grid-stack-item-content'
-    },
-    children: [ // or call load()/addWidget() with same data
-      { x: 1, y: 0, content: 'Item 2' },
-      { x: 0, y: 1, content: 'Item 3' },
-    ]
+    }
   };
 
   mode: GridMode = "edit";
@@ -96,9 +104,9 @@ export class AddPageComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     // Initialize GridStack after the view has been initialized
-    this.grid = GridStack.init(this.gridStackOptions);
-    this.grid.enableMove(true);
-    this.grid.enableResize(true);
+  //  this.grid = GridStack.init(this.gridStackOptions);
+ //   this.grid.enableMove(true);
+   // this.grid.enableResize(true);
     
   }
 
@@ -109,7 +117,7 @@ export class AddPageComponent implements OnInit, AfterViewInit {
     //  handle: '.newWidget',
     //  appendTo: 'body',
     //  helper: 'clone',
-      
+
     //});
     // Initialize logic on component initialization
     this.widgetService.getAllWidget().subscribe({
@@ -125,24 +133,24 @@ export class AddPageComponent implements OnInit, AfterViewInit {
     // Sample data for advanced layout
     const advanced = [
       // ...
-      { x: 0, y: 0, w: 4, h: 2, content: '1' },
+      { x: 0, y: 0, w: 4, h: 2, content: '1', id: '1' },
       // ... (other items)
-      { x: 10, y: 4, w: 2, h: 2, content: '11' }
+      { x: 10, y: 4, w: 2, h: 2, content: '11', id: '2' }
     ];
 
     // Initialize advanced GridStack layout
-    const advGrid = GridStack.init(this.gridStackOptions
-, '#advanced-grid')
+    const grid = GridStack.init(this.gridStackOptions
+      , '#advanced-grid')
       .load(advanced);
 
-  //  var grid = $('.grid-stack').data('gridstack');
+    //  var grid = $('.grid-stack').data('gridstack');
     //var html = '<div>'
     //html += '<div class="grid-stack-item-content">';
     //html += '<div class="col-md-3"> <label> Sample Textbox </label></div>';
     //html += '<div class="col-md-9"> <input type="text" class="form-control" /> </div>';
     //html += '</div></div>';
 
-   // advGrid.addWidget(html, { w: 3 ,h:4})
+    //grid.addWidget(html, { w: 3 ,h:4})
     //advGrid.addWidget({ x: 0, y: 0, minW: 1, content: 'Item d' })
     /*advGrid.on('dragstart', function (event, previousWidget, newWidget) {
       if (event) {
@@ -157,17 +165,17 @@ export class AddPageComponent implements OnInit, AfterViewInit {
       }
     });
 */
-    
-
-   //advGrid.on('dropped', this.gridStackDropped.bind(this));
-   // advGrid.load(this.items); // and load our content directly (will create DOM)
-
-  /*  advGrid.on("dragstop", function (event, previousWidget, newWidget) {
-      // Restore the original content when dragging stops
-      console.log('gridstack dragstop: ', event.target);
 
 
-    });*/
+    //advGrid.on('dropped', this.gridStackDropped.bind(this));
+    //grid.load(this.items); // and load our content directly (will create DOM)
+
+    /* grid.on("dragstop", function (event, previousWidget, newWidget) {
+        // Restore the original content when dragging stops
+        console.log('gridstack dragstop: ', event.target);
+  
+  
+      });*/
 
     //advGrid.on("dropped", function (event, previousWidget, newWidget) {
     //  // Restore the original content when dragging stops
@@ -175,19 +183,133 @@ export class AddPageComponent implements OnInit, AfterViewInit {
 
 
     //});
-    advGrid.on("drag", function (event, previousWidget, newWidget) {
-      // Restore the original content when dragging stops
 
+    //advGrid.on('change', function (event, previousWidget, newWidget) {
+    //  if (previousWidget) {
+    //    var movedItem = previousWidget | undefined;
+    //      // Convert the GridStackNode to a GridStackElement
+    //      const element: GridStackElement = {
+    //        el: movedItem.el,
+    //        x: movedItem.x,
+    //        y: movedItem.y,
+    //        w: movedItem.w,
+    //        h: movedItem.h,
+    //      };
+
+    //      // Remove the widget from the GridStack layout using the converted element
+    //     grid.removeWidget(element);
+    //    }
+
+    //});
+
+
+
+
+
+
+
+   grid.on("dropped", function (event, previousWidget, newWidget) {
+      // Restore the original content when dragging stops
+    //  var serializedFull = grid.save(true, true);
+    //  var serializedData = serializedFull;
+     // console.log(serializedFull);
+     // console.log(serializedData);
+      var ids = 1;
       
+     // this.gridComp?.grid?.addWidget({ x: 3, y: 0, w: 2, content: `item ${ids}`, id: String(ids++) });
+
+     const widgetdata = 
+       { x: 0, y: 0, w: 4, h: 2, content: 'dineshl', id: "23" };
+
+    // widgetdata = widgetdata + '<div class="text-center card text-white grid-stack-item newWidget add id=12"> \
+    //  <div class="card-body grid-stack-item-content add" id=13 > \
+    //    <div style="background-color:black" > \
+    //    <span>' + "dinesh33" + '</span> </div> </div> </div>';
+
+
+
+     grid.addWidget(widgetdata);
+
+      // Assuming you have a Gridstack instance
+     // Process each widget item
+   //  grid.engine.nodes.forEach(node => { console.log("hello ", node.id)
+    //   });
      
+     const removeEl = grid.engine.nodes.find((n) => n.id == "1")?.el;
+      console.log(removeEl);
+     
+
+     // alert("heloo");
+     if (removeEl) grid.removeWidget(removeEl);
+
+      //const widgetId = '12'; // Replace with the actual ID or unique identifier of the widget
+      //const widgetToRemove = document.getElementById(widgetId) as GridStackElement;
+
+      //if (typeof widgetToRemove === 'string' || widgetToRemove instanceof HTMLElement) {
+      //  // If the widget is a string (ID) or HTMLElement, remove it from the grid
+      // grid.removeWidget(widgetdata);
+      //} else {
+      //  console.error(`Widget with ID ${widgetId} not found.`);
+      //}
+
+      // var  el = $('#divGridstackContent').closest('.grid-stack-item').children;
+
+
+    
+
+
+    // grid.removeWidget({ w: 3, h: 4 })
+   //grid.addWidget({ x: 0, y: 0, minW: 1, content: 'Item d' })
+
+  //   grid.removeAll();
+      //var remove = previousWidget as GridItemHTMLElement| undefined;
+     //grid.removeWidget(remove);
+  //   grid.addWidget(widgetdata, { w: 3, h: 4 });
+     //grid.removeWidget(widgetdata)
+    //advGrid.addWidget({ x: 0, y: 0, minW: 1, content: 'Item d' })
+
+     
+
+      //const element: GridStackElement = {
+
+      //  el: previousWidget.el as GridItemHTMLElement ,
+
+      //  x: previousWidget.x ?? 0,
+
+      //  y: previousWidget.y ?? 0,
+
+      //  w: previousWidget.w ?? 1,
+
+      //  h: previousWidget.h ?? 1,
+
+      //};
+
+      // Assuming you have a Gridstack instance
+      //var grid = GridStack.init();
+
+      // Identify the widget by its data attributes (x, y, width, and height)
+      var widgetData = {
+        x: 1,      // Replace with the actual x-coordinate of the widget
+        y: 2,      // Replace with the actual y-coordinate of the widget
+        width: 3,  // Replace with the actual width of the widget
+        height: 4, // Replace with the actual height of the widget
+      };
+
+     
+      // Remove the widget from the grid
+      //advGrid.removeWidget(widgetData);
+
+      // Remove the widget from the GridStack layout using the converted element
+
+     //grid.removeWidget(element);
 
 
   
      
-      var previousContent = $(previousWidget).find('.grid-stack-item-content');
-      console.log(previousContent);
+    //  var previousContent = $(previousWidget).find('.grid-stack-item-content');
+     // console.log(previousContent);
       // Modify the content of the previous widget while dragging
-      previousContent.text('Dragging...');
+    //  previousContent.text('Dragging...');
 
     });
 
@@ -208,8 +330,8 @@ export class AddPageComponent implements OnInit, AfterViewInit {
 
     });
 
-    advGrid.enableMove(true);
-    advGrid.enableResize(true);
+   grid.enableMove(true);
+   grid.enableResize(true);
 
 
   }
